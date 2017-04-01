3ec6f3ba2bc0df389573b18b9d08c80739beec76
oliverjam <oliverjamesphillips@gmail.com>
2017-04-01 10:08:57 +0100

    Initialise Handlebars
    Relates #1

--- src/server.js
+++ src/server.js
@@ -1,10 +1,22 @@
+const path = require('path');
 const server = require('express')();
-// const server = express();
+const hbs = require('express-handlebars');
 
 const port = process.env.PORT || 3000;
 
+server.engine('.hbs', hbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+}));
+
+server.set('view engine', '.hbs');
+server.set('views', path.join(__dirname, 'views'));
+
 server.get('/', (req, res) => {
-  res.send('Yo');
+  res.render('home', {
+    name: 'Oli',
+  });
 });
 
 server.listen(port, (err) => {
